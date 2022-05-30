5070fc6decb402745b711f2678aad0b5867f8e83
Lars André Møen <post@larsandre.no>
2022-05-30 19:17:40 +0200

    ✨ get and post endpoints

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from typing import Union
-
+from pydantic import BaseModel
 from fastapi import FastAPI
+
+import random
+
+tueets = []
+
+
+class Tueet(BaseModel):
+    text: str
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/get/tueets")
+def get_tueets():
+
+    # TODO open local file
+    random_tueet = random.choice(tueets)
+    return {"data": random_tueet}
+
+
+@app.post("/post/tueet")
+def read_root(tueet: Tueet):
+    print("tueet ", tueet)
+
+    tueets.append({"text": tueet.text, "id": len(tueets)})
+    # TODO save locally
+    return {"success": True}
